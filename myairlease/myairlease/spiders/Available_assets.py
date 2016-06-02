7fc9ca064dd32427cd3772ec4e3433ac9abed37c
dianjuar <dianjuar@gmail.com>
2016-06-02 01:57:30 -0400

    Getting the link of categories

--- myairlease/myairlease/spiders/Available_assets.py
+++ myairlease/myairlease/spiders/Available_assets.py
@@ -20,40 +20,55 @@ class AvailableAssetsSpider(scrapy.Spider):
     # Constructor
     def __init__ (self):
     	self.nameOfFile = 'Available_assets_list.csv'
-        self.categoryNames = list()
+        self.categories = list()
 
     def parse(self, response):
     	hxs = Selector(response)
 
     	#get the categories
-        # self.get_niceCategoryNames( hxs )
-        self.get_unguaranteedCategoryNames( hxs )
+        self.get_niceCategoryName( hxs=hxs, response=response )
+        # self.get_unguaranteedCategoryNames( hxs )
 
-        # for x in self.categoryNames:
-            # print ( x )
+        for x in self.categories:
+            print ( x['name'] )
+            print ( x['link'] )
+            print ( '---------' )
 
-    def get_niceCategoryNames(self, hxs):
-        categoryNames_hxs = hxs.xpath('//td[@id="links9"]//h4')
 
-        for cat in categoryNames_hxs:
-            catNames            = cat.xpath('.//descendant-or-self::*/text()').extract()
-            self.categoryNames.append( ','.join(catNames) )
+    def get_niceCategoryName(self, hxs, response):
+        categories_hxs = hxs.xpath('//td[@id="links9"]//h4')
 
+        for cat in categories_hxs:
+            catNames     = cat.xpath('.//descendant-or-self::*/text()').extract()
+            catLink     = cat.xpath('.//a/@href' ).extract()
+            
+            # pdb.set_trace()
+
+            cat         = dict()
+            cat['name'] = ','.join(catNames)
+            cat['link'] = response.urljoin( catLink[0] )
+
+            self.categories.append( cat )
+
+    '''
+    At http://www.myairlease.com/available/available_for_lease there is a no guarantee data base. 
+    This function scrap that links
+    '''
     def get_unguaranteedCategoryNames(self, hxs):
         print('----------*-*--------------------*-*-*-')
         base = '//td[@id="links9"]//p'
-        # categoryNames_hxs = hxs.xpath( base + '/*[ not( self::br or self::b ) ] | ' +
+        # categories_hxs = hxs.xpath( base + '/*[ not( self::br or self::b ) ] | ' +
         #                                base + '/*[ not( parent::br ) ]/text()' )
-        # categoryNames_hxs = hxs.xpath( base + '/descendant-or-self::*[ not( self::b or self::br ) ]/text()')
-        categoryNames_hxs = hxs.xpath( base + '/descendant-or-self::*[ not( self::b or self::br ) ]/text()').extract()
-        
+        # categories_hxs = hxs.xpath( base + '/descendant-or-self::*[ not( self::b or self::br ) ]/text()')
+        categories_hxs = hxs.xpath( base + '/descendant-or-self::*[ not( self::b or self::br ) ]/text()').extract()
+        categoryNamesLINKS_hxs = hxs.xpath( base + '/a/@href').extract()
         # I don't need the first two elements
-        del categoryNames_hxs[0:2]
+        del categories_hxs[0:2]
 
         #remove white spaces
-        self.eraseWhiteSpaces( categoryNames_hxs )
+        self.eraseWhiteSpaces( categories_hxs )
 
-        for x in categoryNames_hxs:
+        for x in categories_hxs:
             print ( x )
             print ( "-")
 
@@ -62,7 +77,6 @@ class AvailableAssetsSpider(scrapy.Spider):
 
     def eraseWhiteSpaces(self, vec):
         
-
         for i, x in enumerate(vec):
             vec[i] = x.strip()
 
