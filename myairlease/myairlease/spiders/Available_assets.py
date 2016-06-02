eb0bf2dcb7507c88a2aa1d7cc52b1512a9f8199a
dianjuar <dianjuar@gmail.com>
2016-06-02 01:27:23 -0400

    Cleaning the category names

--- myairlease/myairlease/spiders/Available_assets.py
+++ myairlease/myairlease/spiders/Available_assets.py
@@ -20,17 +20,51 @@ class AvailableAssetsSpider(scrapy.Spider):
     # Constructor
     def __init__ (self):
     	self.nameOfFile = 'Available_assets_list.csv'
+        self.categoryNames = list()
 
     def parse(self, response):
     	hxs = Selector(response)
 
-    	item = AvailableAssets_Item()
-
     	#get the categories
-    	categoryNames_hxs = hxs.xpath('//td[@id="links9"]//h4')
+        # self.get_niceCategoryNames( hxs )
+        self.get_unguaranteedCategoryNames( hxs )
 
-    	for cat in categoryNames_hxs:
+        # for x in self.categoryNames:
+            # print ( x )
+
+    def get_niceCategoryNames(self, hxs):
+        categoryNames_hxs = hxs.xpath('//td[@id="links9"]//h4')
+
+        for cat in categoryNames_hxs:
             catNames            = cat.xpath('.//descendant-or-self::*/text()').extract()
-            item['Category']    = ','.join(catNames)
-            
+            self.categoryNames.append( ','.join(catNames) )
+
+    def get_unguaranteedCategoryNames(self, hxs):
+        print('----------*-*--------------------*-*-*-')
+        base = '//td[@id="links9"]//p'
+        # categoryNames_hxs = hxs.xpath( base + '/*[ not( self::br or self::b ) ] | ' +
+        #                                base + '/*[ not( parent::br ) ]/text()' )
+        # categoryNames_hxs = hxs.xpath( base + '/descendant-or-self::*[ not( self::b or self::br ) ]/text()')
+        categoryNames_hxs = hxs.xpath( base + '/descendant-or-self::*[ not( self::b or self::br ) ]/text()').extract()
+        
+        # I don't need the first two elements
+        del categoryNames_hxs[0:2]
+
+        #remove white spaces
+        self.eraseWhiteSpaces( categoryNames_hxs )
+
+        for x in categoryNames_hxs:
+            print ( x )
+            print ( "-")
+
+        print('----------*-*--------------------*-*-*-')
         pass
+
+    def eraseWhiteSpaces(self, vec):
+        
+
+        for i, x in enumerate(vec):
+            vec[i] = x.strip()
+
+            if re.compile("\n").search( x ):
+                del vec[i]    
